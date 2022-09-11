c64fd1fe650b6d7e3942d7cdc9c5f11e97585cd6
Vince <vincivenv@gmail.com>
2022-09-11 23:04:33 +0200

    write video func, start position

--- flow.py
+++ flow.py
@@ -1,25 +1,24 @@
 from pathlib import Path
 
 import fire
-import torch
 import torchvision
 import torchvision.transforms.functional as F
 from torchvision.io import write_jpeg
 from torchvision.models.optical_flow import Raft_Large_Weights, raft_large
 from torchvision.utils import flow_to_image
-import os
+
+from stitch import write_video
 
 
-def runner(video, to):
+def runner(video, to, ss=0, filename="dummy.mp4"):
     video = Path(video)
-    vftames, _, metadata = torchvision.io.read_video(
-        str(video), pts_unit="sec", output_format="TCHW", end_pts=to
+    vframes, aframes, metadata = torchvision.io.read_video(
+        str(video), pts_unit="sec", output_format="TCHW", start_pts=ss, end_pts=to
     )
-    print(metadata)
     model, transforms = flow_model()
     dest_dir = Path("flows")
     dest_dir.mkdir(exist_ok=True, parents=True)
-    for idx, (img1, img2) in enumerate(zip(vftames, vftames[1:])):
+    for idx, (img1, img2) in enumerate(zip(vframes, vframes[1:])):
         img1, img2 = preprocess(
             img1=img1.unsqueeze(0), img2=img2.unsqueeze(0), transforms=transforms
         )
@@ -27,6 +26,7 @@ def runner(video, to):
         pred_flow = flows[-1][0]
         flow_img = flow_to_image(pred_flow).to("cpu")
         write_jpeg(flow_img, str(dest_dir / f"predicted_flow_{idx}.jpg"))
+    write_video(dest_dir, filename, aframes, metadata)
 
 
 def flow_model():
@@ -42,11 +42,10 @@ def preprocess(img1, img2, transforms):
     img2 = F.resize(img2, size=[520, 960])
     return transforms(img1, img2)
 
-def gen_video(flow_img):
-    
-    ...
+
 if __name__ == "__main__":
     # os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
     # device = "mps" if torch.backends.mps.is_available() else "cpu"
     device = "cpu"
     fire.Fire(runner)
+# ffmpeg -i right.mp4 -vf "fps=24,scale=960:520:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" right.gif
